63b916e1633bd8060aad00a6e690032488a491c6
lilirawo236 <lilirawo236@ed.amdsb.ca>
2017-01-13 13:24:30 -0500

    Initial Commit

--- MASH/Form1.Designer.cs
+++ MASH/Form1.Designer.cs
@@ -66,7 +66,7 @@
             // 
             this.careerLabel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.careerLabel1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.careerLabel1.Location = new System.Drawing.Point(21, 18);
+            this.careerLabel1.Location = new System.Drawing.Point(22, 18);
             this.careerLabel1.Name = "careerLabel1";
             this.careerLabel1.Size = new System.Drawing.Size(92, 45);
             this.careerLabel1.TabIndex = 2;
