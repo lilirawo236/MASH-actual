b2526499b3369dffda03d3d31f82a27c3a70e8b4
wahhe <yuanhe326@ed.amdsb.ca>
2017-01-17 13:26:57 -0500

    nothing

--- MASH/Form1.Designer.cs
+++ MASH/Form1.Designer.cs
@@ -407,11 +407,11 @@
             // 
             // outputLabel1
             // 
-            this.outputLabel1.AutoSize = true;
-            this.outputLabel1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputLabel1.Location = new System.Drawing.Point(39, 420);
+            this.outputLabel1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel1.ForeColor = System.Drawing.Color.Maroon;
+            this.outputLabel1.Location = new System.Drawing.Point(29, 391);
             this.outputLabel1.Name = "outputLabel1";
-            this.outputLabel1.Size = new System.Drawing.Size(0, 22);
+            this.outputLabel1.Size = new System.Drawing.Size(348, 71);
             this.outputLabel1.TabIndex = 32;
             // 
             // nextButton
